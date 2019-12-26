b8ac2b93436f3c8f9ae650757dc429b2b7e09b47
Raian de Andrades <raian.andrades@gmail.com>
2019-12-26 19:07:05 -0300

    Estilização da autenticação

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
   return (
-    <h1>Login</h1>
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Seu senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
+    </>
   );
 }
