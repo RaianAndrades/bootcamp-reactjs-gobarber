1acf03daa1156ad399f16892804f4e5b8c1b2ac2
Raian de Andrades <raian.andrades@gmail.com>
2019-12-28 01:19:26 -0300

    Página de perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+
+import { Container } from './styles';
 
 export default function Profile() {
+  const profile = useSelector((state) => state.user.profile);
+
+  function handleSubmit(data) {
+
+  }
+
   return (
-    <div />
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+        <hr />
+
+        <Input name="oldPassword" type="password" placeholder="Sua senha atual" />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input name="confirmPassword" type="password" placeholder="Confirmação de senha" />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="submit">Sair do GoBarber</button>
+    </Container>
   );
 }
