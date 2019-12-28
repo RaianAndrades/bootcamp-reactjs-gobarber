277f69064659255629441c2f3a5d31a7e42a72ae
Raian de Andrades <raian.andrades@gmail.com>
2019-12-28 20:50:43 -0300

    Foto de perfil

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -9,18 +9,18 @@ import { updateProfileSuccess, updateProfileFailure } from './actions';
 
 export function* updateProfile({ payload }) {
   try {
-    const { name, email, ...rest } = payload.data;
+    const {
+      name, email, avatar_id, ...rest
+    } = payload.data;
 
-    const profile = Object.assign({
-      name,
-      email,
-    },
-    ...rest.oldPassword ? rest : {});
+    const profile = Object.assign(
+      { name, email, avatar_id },
+      ...(rest.oldPassword ? rest : {}),
+    );
 
     const response = yield call(api.put, 'users', profile);
 
     toast.success('Perfil atualizado com sucesso!');
-
     yield put(updateProfileSuccess(response.data));
   } catch (err) {
     toast.error('Erro ao atualizar perfil, confira seus dados.');
@@ -28,6 +28,4 @@ export function* updateProfile({ payload }) {
   }
 }
 
-export default all([
-  takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile),
-]);
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
